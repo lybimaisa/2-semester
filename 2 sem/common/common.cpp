ffea964366ebdc41d2dd13b37eefc42a7d4149fe
lybimaisa <zanfurry2006@gmail.com>
2025-04-14 18:59:26 +0300

    lab0

---
+++ 2 sem/common/common.cpp
@@ -0,0 +1,11 @@
+﻿// common.cpp : Определяет функции для статической библиотеки.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "stack.h"
+
+// TODO: Это пример библиотечной функции.
+void fncommon()
+{
+}
