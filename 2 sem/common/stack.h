20c0078a2a2418221023aae64b2838bdb09b4e98
lybimaisa <zanfurry2006@gmail.com>
2025-05-14 22:31:36 +0300

    lab1

---
+++ 2 sem/common/stack.h
@@ -0,0 +1,99 @@
+#pragma once
+
+#include <stdexcept>
+
+using namespace std;
+
+/// <summary>
+/// Класс для работы со стеком (LIFO - последний вошел, первый вышел).
+/// </summary>
+template <typename T>
+class Stack
+{
+private:
+    struct Node
+    {
+        T data;
+        Node* next;
+    };
+
+    Node* top;
+    int size;
+
+public:
+    /// <summary>
+    /// Конструктор по умолчанию
+    /// </summary>
+    Stack() = default;
+
+    /// <summary>
+    /// Удаляет все элементы из стека.
+    /// </summary>
+    ~Stack()
+    {
+        clear();
+    }
+
+    /// <summary>
+    /// Добавляет элемент на вершину стека.
+    /// </summary>
+    void push(T value)
+    {
+        Node* temp = new Node();
+        temp->data = value;
+        temp->next = top;
+        top = temp;
+        size++;
+    }
+
+    /// <summary>
+    /// Удаляет верхний элемент из стека и возвращает его значение.
+    /// </summary>
+    T pop()
+    {
+        if (top == nullptr)
+        {
+            throw runtime_error("Stack is empty!");
+        }
+        Node* temp = top->next;
+        T data = top->data;
+        delete top;
+        top = temp;
+        size--;
+        return data;
+    }
+
+    /// <summary>
+    /// Возвращает значение верхнего элемента стека. Удаления не происходит.
+    /// </summary>
+    T peek() const
+    {
+        if (top == nullptr)
+        {
+            throw runtime_error("Stack is empty!");
+        }
+        return top->data;
+    }
+
+    /// <summary>
+    /// Возвращает текущее количество элементов в стеке.
+    /// </summary>
+    int count() const
+    {
+        return size;
+    }
+
+    /// <summary>
+    /// Очищает стек, удаляя все элементы.
+    /// </summary>
+    void clear()
+    {
+        while (top != nullptr)
+        {
+            Node* temp = top->next;
+            delete top;
+            top = temp;
+        }
+        size = 0;
+    }
+};
